5d53115e405d9ba6886dfe4790ade384ec2a30c7
Anna <44573387+annfike@users.noreply.github.com>
2021-09-13 17:19:59 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -4,12 +4,12 @@ load_dotenv()
 
 DATABASES = {
     'default': {
-        'DB_ENGINE': os.getenv("DB_ENGINE"),
-        'DB_HOST': os.getenv("DB_HOST"),
-        'DB_PORT': os.getenv("DB_PORT"),
-        'DB_NAME': os.getenv("DB_NAME"),
-        'DB_USER': os.getenv("DB_USER"),
-        'DB_PASSWORD': os.getenv("DB_PASSWORD"),
+        'ENGINE': os.getenv("DB_ENGINE"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
     }
 }
 
